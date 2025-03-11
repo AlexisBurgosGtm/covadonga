1726fc1666bb909cad32f325c59c97a4958e1211
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2025-03-11 14:27:39 -0600

    cambiso varios

--- build/controllers/CustomerVars.js
+++ build/controllers/CustomerVars.js
@@ -2,16 +2,6 @@
 //data_empresa_config.TIPO_PRECIO
 
 
-let tipo_precios =[
-                {tipo:"PRECIO",nombre:"PUBLICO"},
-                {tipo:"PRECIO_A",nombre:"PRECIO A"},
-                {tipo:"PRECIO_B",nombre:"PRECIO B"},
-                {tipo:"PRECIO_C",nombre:"PRECIO C"},
-                {tipo:"PRECIO_D",nombre:"PRECIO D"},
-                {tipo:"PRECIO_E",nombre:"PRECIO E"},
-                {tipo:"PRECIO_F",nombre:"PRECIO F"}
-            ];
-
 let tipo_empleados = [
     {codigo:1,descripcion:"GERENCIA"},
     {codigo:2,descripcion:"SUPERVISION"},
@@ -48,14 +38,7 @@ let tipo_negocios = [
     { tipo: 'VARIEDADES', descripcion: 'VARIEDADES'}
 ]
 
-function get_tipo_precios(){
 
-    let str = '' //"<option value=''>Seleccione una sucursal</option>";
-    tipo_precios.map((r)=>{
-        str += `<option value='${r.tipo}'>${r.nombre}</option>`
-    });
-    return str;
-}
 
 
 
